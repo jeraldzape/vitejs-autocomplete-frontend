1f664a62a7dee45932df0f234906542af683f2da
Jerald Zape <jeraldzape@gmail.com>
2022-12-11 23:42:10 +0800

    Fixed look ahead blur issue

--- src/components/AutoComplete.vue
+++ src/components/AutoComplete.vue
@@ -3,36 +3,39 @@
         <div class="w-100 header-wrapper p-3 pb-0">
             <div class="container">
                 <div class="jumbotron text-center text-white py-4">
-                    <h1>Books Directory</h1>
+                    <h2>Books Directory</h2>
                     <p>Lorem ipsum dolor sit amet</p>
                 </div>
             </div>
             <div class="search-wrapper mx-auto position-relative">
                 <form class="form-inline" @submit="doSubmit">
-                    <div class="input-group">
-                        <input type="text" class="form-control" v-model="searchQuery" @focus="lookAheadState = true" @input="doSearch()"
-                            autocomplete="off" placeholder="Type book title or author here" required>
-                            <button type="submit">
-                                <i class="icon icon-search"></i>
-                            </button>
+                    <div class="input-group position-relative">
+                        <input type="text" class="form-control" v-model="searchQuery" @focus="lookAheadState = true" @blur="onLookAheadBlur()"
+                            @input="doSearch()" autocomplete="off" placeholder="Type book title or author here"
+                            required>
+                        <button type="submit">
+                            <font-awesome-icon icon="magnifying-glass" />
+                        </button>
                     </div>
                 </form>
                 <div v-if="lookAheadState && booksLookAhead.length > 0"
                     class="search-wrapper__suggestions w-100 bg-white text-left">
                     <ul class="list-unstyled mb-0 w-100 h-100 overflow-auto scrollable">
-                        <li @click="setSearch(book.name.toLowerCase())" v-for="book in booksLookAhead" v-html="highlightQuery(book.name)"></li>
+                        <li @click="setSearch(book.name.toLowerCase())" v-for="book in booksLookAhead"
+                            v-html="highlightQuery(book)"></li>
                     </ul>
                 </div>
             </div>
         </div>
         <div class="flex-fill h-100 w-100 px-3 pb-3 pt-5 results-wrapper overflow-auto scrollable">
             <div class="container h-100 position-relative">
-                <ul class="list-unstyled text-left d-flex flex-row flex-wrap justify-content-start p-2 position-relative">
+                <ul
+                    class="list-unstyled text-left d-flex flex-row flex-wrap justify-content-start p-2 position-relative">
                     <li class="result-item overflow-hidden" v-for="book in books">
                         <div class="card border-0">
                             <div class="card-body">
                                 <h6 class="card-title">{{ book.name }}</h6>
-                                <p class="card-text small text-muted"><i>by: {{ book.author }}</i></p>
+                                <p class="card-text small text-muted"><i>—{{ book.author }}</i></p>
                                 <!-- <a href="javascript:" class="btn btn-primary">Read</a> -->
                             </div>
                         </div>
@@ -52,10 +55,8 @@ export default {
             lookAheadState: false,
             booksLookAhead: [],
             books: [],
-            state: {
-                isSearching: false,
-                searchTimer: null
-            }
+            searchTimer: null
+
         }
     },
     methods: {
@@ -64,42 +65,45 @@ export default {
             this.lookAheadState = false
             this.books = this.booksLookAhead
         },
-        setSearch(value){
+        setSearch(value) {
+            console.log("Set ", value)
             this.searchQuery = value
             this.lookAheadState = false
             this.getBooks(-1)
         },
         doSearch() {
-            if(!this.searchQuery){
+            if (!this.searchQuery) {
                 return false
             }
             this.lookAheadState = true
 
-            if (this.state.searchTimer) {
-                clearTimeout(this.state.searchTimer);
+            if (this.searchTimer) {
+                clearTimeout(this.searchTimer);
             }
-            this.state.searchTimer = setTimeout(() => {
+            this.searchTimer = setTimeout(() => {
                 this.getBooks(10)
             }, 300);
         },
-        highlightQuery(query){
+        highlightQuery(book) {
             let regex = new RegExp(this.searchQuery, 'ig')
-            return query.toLowerCase().replace(regex, '<b>$&</b>')
+            let bookName = book.name.toLowerCase().replace(regex, '<b>$&</b>')
+            let author = '<small class="text-muted">'+ book.author.toLowerCase().replace(regex, '<b>$&</b>') +'</small>'
+            return bookName + ' —' + author
         },
         getBooks(limit) {
             fetch('http://127.0.0.1:8000/api/books?q=' + this.searchQuery)
                 .then(response => response.json())
                 .then(data => {
                     this.booksLookAhead = limit > -1 ? data.slice(0, limit) : data
-                    // if(limit > -1){
-                    //     let regex = new RegExp(this.searchQuery.toLowerCase(), 'ig')
-                    //     this.booksLookAhead = data.filter((item) => {
-                    //         return item.name.replace(regex, '<b>$&</b>')
-                    //     })
-                    // }else{
-                    //     this.books = data
-                    // }
+                    if(limit == -1){
+                        this.books = data
+                    }
                 })
+        },
+        onLookAheadBlur(){
+            setTimeout(() => {
+                this.lookAheadState = false
+            }, 200)
         }
     }
 }
@@ -124,7 +128,7 @@ export default {
             border-radius: 12px;
             padding: 12px;
 
-            ul{
+            ul {
                 max-height: 300px;
                 overflow: auto;
                 padding: 6px;
@@ -146,44 +150,65 @@ export default {
             }
         }
 
-        input[type=text] {
-            height: 56px;
-            border-radius: 50px;
-            padding: 16px 32px;
+        .input-group {
             box-shadow: 0 3px 10px rgba(0, 0, 0, 0.15);
-            border: 0;
+            border-radius: 50px;
+            overflow: hidden;
 
-            &:focus,
-            &:active {
-                outline: 0;
+            input[type=text] {
+                height: 56px;
+                padding: 16px 32px;
+                border: 0;
+            }
+            
+
+            button[type="submit"] {
+                background: white;
+                border: 0;
+            }
+            input[type=text],
+            button[type=submit]{
+                &:focus,
+                &:hover,
+                &:active {
+                    outline: 0;
+                    box-shadow: none;
+                    border: 0;
+                }
             }
         }
+
     }
 }
 
 .results-wrapper {
     background: var(--bs-gray-200);
     z-index: 0;
-    &.scrollable{
+
+    &.scrollable {
         &::-webkit-scrollbar-track {
-        background: var(--bs-gray-300);
-    }
+            background: var(--bs-gray-300);
+        }
     }
 
     ul {
         .result-item {
             padding: 6px;
             width: 100%;
-            @media (min-width: 576px){
+
+            @media (min-width: 576px) {
                 width: 50%;
             }
-            @media (min-width: 768px){
+
+            @media (min-width: 768px) {
                 width: 33.33%;
             }
-            @media (min-width: 992px){
+
+            @media (min-width: 992px) {
                 width: 25%;
             }
-            .card{
+
+            .card {
                 border-radius: 16px;
                 min-height: 100%;
             }
@@ -200,7 +225,7 @@ export default {
         border-radius: 500px;
         background: #7c93f3;
     }
-    
+
     &::-webkit-scrollbar-track {
         background: #f1f1f1;
     }
--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faMagnifyingGlass)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
+ 
